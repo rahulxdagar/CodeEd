09f9f9ce826886cdc63c0ae72fba2c4266ac5fb7
Rahul Dagar <rahulxdagar@gmail.com>
2021-07-17 22:10:32 +0530

    Updates

---
+++ precache-manifest.7bc6619996d5cba6db45f5ae60549348.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5661edb257144bb45c1199e182b0d6ff",
+    "url": "/CodeEd/index.html"
+  },
+  {
+    "revision": "16af635f997aa470cf56",
+    "url": "/CodeEd/static/css/2.8ab860fe.chunk.css"
+  },
+  {
+    "revision": "306a9ac6a3b297c2ef9a",
+    "url": "/CodeEd/static/css/main.5b60b98c.chunk.css"
+  },
+  {
+    "revision": "16af635f997aa470cf56",
+    "url": "/CodeEd/static/js/2.0007d213.chunk.js"
+  },
+  {
+    "revision": "64f54ce285484345cd5e3bd96b120148",
+    "url": "/CodeEd/static/js/2.0007d213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "306a9ac6a3b297c2ef9a",
+    "url": "/CodeEd/static/js/main.045f38bb.chunk.js"
+  },
+  {
+    "revision": "5eee781195514ffcfdb3",
+    "url": "/CodeEd/static/js/runtime-main.8cdb86cd.js"
+  }
+]);
